9e0ed97c10234753bce23e8cc5b698e538c08119
Rohan Prakash <pksh.rohan16@gmail.com>
2025-01-31 11:40:39 +0530

    Some changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,31 @@
-// import { Routes, Route } from "react-router-dom";
-// import Header from "./components/Header";
-// import Footer from "./components/Footer";
-// import Home from "./pages/Home";
-// import Complaints from "./pages/Complaints";
-// import Status from "./pages/Status";
-// import Contact from "./pages/Contact";
-
-// function App() {
-//   return (
-//     <div className="flex flex-col min-h-screen bg-gray-100">
-//       <Header />
-//       <div className="flex-grow">
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/complaints" element={<Complaints />} />
-//           <Route path="/status" element={<Status />} />
-//           <Route path="/contact" element={<Contact />} />
-         
-//         </Routes>
-//       </div>
-//       <Footer />
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
-import Sidebar from "./components/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Complaints from "./pages/Complaints";
+import Status from "./pages/Status";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
-    
-      <Sidebar />
-    
+    <div className="flex flex-col min-h-screen bg-gray-100">
+      <Header />
+      <div className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/complaints" element={<Complaints />} />
+          <Route path="/status" element={<Status />} />
+          <Route path="/contact" element={<Contact />} />
+         
+        </Routes>
+      </div>
+      <Footer />
+    </div>
   );
 }
 
 export default App;
+
+
+
+
