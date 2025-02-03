4c1186b8f1db43ec824bd4d0d025bc3cca783a02
sarthaknayyar <sarthaknayyar6@gmail.com>
2025-02-03 21:28:37 +0530

    mongo connected

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const {connectDB} = require('./connection');
+const mongouri = process.env.MONGO_URI;
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,15 +12,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Database Connection
-mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
+// Connect to MongoDB
+connectDB(mongouri);
 
 // Routes
 // const grievanceRoutes = require("./routes/grievances");
-app.use("/api/grievances", grievanceRoutes);
+// app.use("/api/grievances", grievanceRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
  
