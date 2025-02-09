3a1c500de8255f5324cfbbd1e09f1e0695d7515f
sarthaknayyar <sarthaknayyar6@gmail.com>
2025-02-09 12:36:06 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5000", // Add your frontend domain here
+    origin: "http://localhost:5173", // Add your frontend domain here
     credentials: true,
     optionsSuccessStatus: 200
 }));
